97459fca4357b7933c5cc6b67f66b46d75e62e10
Koushik Pusarla <koushikpusarla07@gmail.com>
2024-08-31 14:03:45 +0530

    Add files via upload

---
+++ Task - 2/project2/app2/models.py
@@ -0,0 +1,12 @@
+# models.py
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.IntegerField()
+    department = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
